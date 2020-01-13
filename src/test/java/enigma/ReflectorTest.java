3bf6b85451b2bc72bca179408f82dbdd1e955f02
nc033877 <nc033877@chichester.esirem-ad.ad.u-bourgogne.fr>
2020-01-13 12:16:39 +0100

    Modification fonction tesReflector

--- src/test/java/enigma/ReflectorTest.java
+++ src/test/java/enigma/ReflectorTest.java
@@ -7,8 +7,14 @@ import org.junit.Test;
 public class ReflectorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void ConvertForward_True() {
+            int alpha[] = new int[26];
+            for (int i = 0; i < 26; i++) {
+                alpha[i] = i;
+            }
+            Reflector reflect = new Reflector(alpha);
+            int conversion = reflect.convertForward(0);
+            assertEquals(conversion, 0);
 	}
 
 }
